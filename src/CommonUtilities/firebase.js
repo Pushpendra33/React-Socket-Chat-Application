ef69207d4f8264fd547f86b4cda076e50a6b91bc
Pushpendra singh <108654034+Pushpendra33@users.noreply.github.com>
2023-06-09 14:25:13 +0530

    Update firebase.js

--- src/CommonUtilities/firebase.js
+++ src/CommonUtilities/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATYwn2kjEXj-vEXqMDHUaJblq2NSZKqyQ",
-  authDomain: "saf3chat-50feb.firebaseapp.com",
-  projectId: "saf3chat-50feb",
-  storageBucket: "saf3chat-50feb.appspot.com",
-  messagingSenderId: "137775410696",
-  appId: "1:137775410696:web:e9839b74c977bed242abbd",
-  measurementId: "G-YDDBDYLJ8L",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
